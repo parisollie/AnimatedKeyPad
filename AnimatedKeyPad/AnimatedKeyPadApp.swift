3e12d2bf7972a015772b5c608f3bbba7dcc1d5de
parisollie <pjff18@gmail.com>
2025-03-25 13:05:52 -0600

    Initial Commit

---
+++ AnimatedKeyPad/AnimatedKeyPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedKeyPadApp.swift
+//  AnimatedKeyPad
+//
+//  Created by Paul F on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedKeyPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
